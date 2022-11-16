fixed bug RMSE computation no-predictor

code/auxiliary/architectures.py
@@ -13,20 +13,18 @@ class CentroidPredictionLSTM(nn.Module):
         output_features (int): number of output features at each time step
         num_layers (int): number of LSTM hidden layers
         batch_size (int): number of data patterns to be fed to network simultaneously
-        seq_len_in (int): length of input window
         seq_len_out (int): length of predicted window
         device (torch.device): torch cuda device
         dropout (float, optional): probability of dropout [0,1] in dropout layer  
         bi (bool, optional): if True, becomes a bidirectional LSTM
     """
     def __init__(self, input_features, hidden_features, output_features,
-                 num_layers, seq_len_in, seq_len_out, 
+                 num_layers, seq_len_out, 
                  device, dropout=0, bi=False):
         super(CentroidPredictionLSTM, self).__init__()
         
         self.hidden_features = hidden_features
         self.num_layers = num_layers
-        self.seq_len_in = seq_len_in
         self.seq_len_out = seq_len_out
         self.output_features = output_features
         self.bi = bi

code/auxiliary/utils.py
@@ -829,8 +829,8 @@ def get_centroids_segmentations(outputs, predictions, to_tensor=True):
     """Get center of mass for output segmentations and predicted segmentations for 250 ms and 500 ms forecast.
 
     Args:
-        outputs (array or tensor): ground truth segmentations with shape (1, 2, 1, ...)
-        predictions (array or tensor): predicted segmentations with shape (1, 2, 1, ...)
+        outputs (array or tensor): ground truth segmentations with shape (b, 2, 1, ...)
+        predictions (array or tensor): predicted segmentations with shape (b, 2, 1, ...)
 
     Returns:
         tuple with floats: centroids positions in SI and AP.
@@ -844,10 +844,15 @@ def get_centroids_segmentations(outputs, predictions, to_tensor=True):
     else:
         pass  
     
-    centroids_output0 = scipy.ndimage.measurements.center_of_mass(outputs[0,0,0,...])
-    centroids_predictions0 = scipy.ndimage.measurements.center_of_mass(predictions[0,0,0,...])
-    centroids_output1 = scipy.ndimage.measurements.center_of_mass(outputs[0,1,0,...])
-    centroids_predictions1 = scipy.ndimage.measurements.center_of_mass(predictions[0,1,0,...])
+    centroids_output0 = []
+    centroids_predictions0 = []
+    centroids_output1 = []
+    centroids_predictions1 = []
+    for el in range(outputs.shape[0]):
+        centroids_output0.append(scipy.ndimage.measurements.center_of_mass(outputs[el,0,0,...]))
+        centroids_predictions0.append(scipy.ndimage.measurements.center_of_mass(predictions[el,0,0,...]))
+        centroids_output1.append(scipy.ndimage.measurements.center_of_mass(outputs[el,1,0,...]))
+        centroids_predictions1.append(scipy.ndimage.measurements.center_of_mass(predictions[el,1,0,...]))
     
     if to_tensor:
         centroids_output0, centroids_predictions0 = torch.tensor(centroids_output0, dtype=torch.float32), torch.tensor(centroids_predictions0, dtype=torch.float32)